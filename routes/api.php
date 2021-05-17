5140ab2a364c66cfa04dd20d9759fb1c69cd0ce9
Jithesh Jose <jitheshjs983@gmail.com>
2021-05-17 19:00:25 +0530

    change for laravel authentication

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$api = app('Dingo\Api\Routing\Router');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api->version('v1'  , function ($api) {
+    $api->get('/home', 'HomeController@index');
 });
+
